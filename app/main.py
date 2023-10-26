cfc500b4dd42ab4dc2edd6889d9df8bb627db777
vaniamaksimov <vaniamaksimov@gmail.com>
2023-10-26 22:48:20 +0300

    bashrc

--- app/main.py
+++ app/main.py
@@ -7,13 +7,23 @@ log = get_logger('main')
 
 def main():
     homedir = Path.home()
+    # os.environ['HISTTIMEFORMAT'] = '%F %T '
     log.info('start application')
-    with open(homedir / '.bash_history') as bash_history:
-        for command in bash_history:
-            yield command
+    file = Path(homedir / '.bashrc')
+    if file.exists() and file.is_file():
+        log.info('file exists')
+    else:
+        log.info('file not exists')
+    # with open(homedir / '.bashrc') as file:
+    #     print(file)
+    # with open(homedir / '.bash_history') as bash_history:
+    #     for command in bash_history:
+    #         yield command
 
 
 if __name__ == '__main__':
-    for i in main():
-        log.info(f'found command {i}')
-        print(i)
+    main()
+    # for i in range(readline.read_history_file('~/.bash_history')):
+    # for i in main():
+    #     # log.info(f'found command {i}')
+    #     print(i)
